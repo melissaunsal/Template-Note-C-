eb5bb8221e64e731e0a6e5382a42d7c2320de2e9
melissaunsal <melissaunsal10@gmail.com>
2022-06-28 14:45:28 +0300

    Proje dosyası ekle.

---
+++ Template Note/Template Note.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+using namespace std;
+template <typename T>
+T incr(T x) {//increase
+    return x + 1;
+
+}
+//yer değiştirme ( class T )
+template <typename T>
+void substitution(T& x, T& y) {
+    T temp = x;
+    x = y;
+    y = temp;
+
+}
+template<class T>
+class myclass {
+public:
+    T data;
+    myclass(T data) : data(data) {
+
+    }
+};
+int main()
+{
+    //datası int yapıldı
+    myclass<int> b(10); //şablon sınıftan nesne oluşturuldu
+    cout << b.data << endl;
+
+
+
+
+    //// değişkenlerimizin değerleri sürekli değişebilir 
+  ///  int a = 5;
+  ///  int b = 10;
+  ///  cout << a << "\t" << b << endl;
+   /// substitution(a, b);
+   /// cout << a << "\t" << b << endl;
+
+
+
+}
